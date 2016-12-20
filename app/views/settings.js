80d570b5a75a77da6f9c084805504ebeca8e693f
deefactorial <deefactorial@gmail.com>
2016-12-20 12:22:29 -0800

    check for .cc instead of dot

--- app/views/settings.js
+++ app/views/settings.js
@@ -228,11 +228,11 @@ module.exports = Marionette.CollectionView.extend({
                       //console.log(csvPosition);
                       //console.log(csvPosition['From']);
                       var from = elements[csvPosition['From']];
-                      if(from.indexOf('.') === -1){
+                      if(from.indexOf('.cc') === -1){
                         from = from + '.' + Self.steward.get('stewardname') + '.cc';
                       }
                       var to = elements[csvPosition['To']];
-                      if(to.indexOf('.') === -1){
+                      if(to.indexOf('.cc') === -1){
                         to = to + '.' + Self.steward.get('stewardname') + '.cc';
                       }
                       var description = elements[csvPosition['Description']];
@@ -462,11 +462,11 @@ module.exports = Marionette.CollectionView.extend({
                       if(row > 0 && elements.length > 1) {
 
                         var from = elements[csvPosition['From']];
-                        if(from.indexOf('.') === -1){
+                        if(from.indexOf('.cc') === -1){
                           from = from + '.' + Self.steward.get('stewardname') + '.cc';
                         }
                         var to = elements[csvPosition['To']];
-                        if(to.indexOf('.') === -1){
+                        if(to.indexOf('.cc') === -1){
                           to = to + '.' + Self.steward.get('stewardname') + '.cc';
                         }
                         var description = elements[csvPosition['Description']];
